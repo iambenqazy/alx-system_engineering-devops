e9e712869cc1310c4eba8fa38887aec307b87bd1
Benjamin Kwame Oduro <iambenqazy@gmail.com>
2024-06-16 19:51:34 +0000

    ups

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
-"""
-Function to query subscribers on a given Reddit subreddit.
-"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Return the total number of subscribers on a given subreddit.
-    
-    Args:
-    subreddit (str): The name of the subreddit to query.
-    
-    Returns:
-    int: The number of subscribers or 0 if subreddit is not found or any error occurs.
-    """
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "python:subreddit.subscriber.counter:v1.0 (by /u/yourusername)"
+        "User-Agent": "User-Agent"
     }
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
-            return 0
-        results = response.json().get("data", {})
-        return results.get("subscribers", 0)
-    except requests.RequestException:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
